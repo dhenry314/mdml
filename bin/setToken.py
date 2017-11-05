5f09c8aa620d36823aadc9c28dfd668328c80939
weboffice <dhenry314@gmail.com>
2017-11-05 10:12:21 -0600

    added setToken.py to create a token as a cron job

---
+++ bin/setToken.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys,argparse,json,os.path
+from collections import namedtuple
+
+configPath = 'config.json'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--configPath')
+args = parser.parse_args()
+
+configPath = args.configPath
+
+if not os.path.isfile(configpath):
+	print "No config.json file found at " + str(configpath)
+	sys.exit()
+
+with open(configpath) as json_data_file:
+    configJ = json.load(json_data_file)
+    config = namedtuple('config',configJ.keys())(**configJ)
+
+tokenPath = str(config.mdmlClientDir) + "_TOKEN"
+mdmlModules = str(config.mdmlCore) + "pythonModules/"
+sys.path.append(mdmlModules)
+
+from mdml import TokenService as ts
+from mdml import Utils as u
+
+try:
+	jwt = ts.getTokenFromConfig(config)
+except ValueError as e:
+        print(e)
+        sys.exit()
+
+if jwt:
+        try:
+              result = u.writeToFile(tokenPath,jwt)
+        except ValueError as e:
+              print(e)
+              sys.exit()
+
+if result:
+        print "JSON Web Token written to _TOKEN."
+
+sys.exit()
+
